e7938cb069f6837421b5958f75017b2325efae14
ilabstaff <ilabstaff@172.16.116.79>
2019-12-16 17:55:20 -0800

    mad libs

---
+++ src/section3/Robin.java
@@ -0,0 +1,20 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class Robin {
+public static void main(String[] args) {
+//	String input = JOptionPane.showInputDialog("robloxand minecraft for days ");
+//	
+//	
+//	JOptionPane.showMessageDialog(null, "hello " + input);
+//	JOptionPane.showMessageDialog(null, "hello peeps");
+//	JOptionPane.showMessageDialog(null, "what you doing");
+	String input = JOptionPane.showInputDialog("Hello what is your name");
+	//JOptionPane.showMessageDialog(null, ("My name is Jayvon"));
+	JOptionPane.showMessageDialog(null, ("I like your hair "+ input));
+	
+	
+	
+}
+}
